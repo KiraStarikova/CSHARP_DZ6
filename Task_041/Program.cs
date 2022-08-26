e4b43faaacc5bfc90324d34feb100c253b253e7a
Kira <starkial@lenta.ru>
2022-08-26 14:04:18 +0500

    Task 41 добавлено решение уравнений

---
+++ Task_041/Program.cs
@@ -0,0 +1,18 @@
+﻿//  Пользователь вводит с клавиатуры M чисел. Посчитайте, 
+//сколько чисел больше 0 ввёл пользователь.
+
+//Console.Write("Введите числа(через пробел): ");
+//int[] array = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+double[] n = Console.ReadLine().Split(' ').Select(e => Convert.ToDouble(e)).ToArray();
+
+double count = 0;
+ 
+for (int i = 0; i < n.Length; i++)
+{
+    if (n[i] > 0)
+    {
+        count++;
+    }
+}
+ 
+Console.WriteLine($"Кол-во элементов > 0: {count}");
